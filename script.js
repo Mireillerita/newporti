20327a0f77688fa95abac1113588f5224dc55db4
Mireillerita <mireillendahimana@gmail.com>
2024-03-17 03:14:28 -0700

    changes

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- // JavaScript for toggling mobile menu
+document.getElementById('menuButton').addEventListener('click', function() {
+    var mobileMenu = document.getElementById('mobileMenu');
+    var closeButton = document.getElementById('closeButton');
+    var menuButton = document.getElementById('menuButton');
+    
+    mobileMenu.classList.toggle('hidden');
+    closeButton.classList.toggle('hidden');
+    menuButton.classList.toggle('hidden');
+});
+document.getElementById('closeButton').addEventListener('click', function() {
+    var mobileMenu = document.getElementById('mobileMenu');
+    var closeButton = document.getElementById('closeButton');
+    var menuButton = document.getElementById('menuButton');
+    
+    mobileMenu.classList.add('hidden');
+    closeButton.classList.add('hidden');
+    menuButton.classList.remove('hidden');
+});
